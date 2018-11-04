85c70274cd2a467f3756b6027ec91111f91e505a
sabrina <hoekehouse@gmail.com>
2018-11-03 17:50:52 -0700

    clean comments

--- working.js
+++ working.js
@@ -10,8 +10,6 @@ $(document).ready(function(){
 	"Make it So",
 	"Sorry, Ask Again"];
 
-
-
 //on button click, prompt with question, store that string as var question, insert question string into div
 	$( "#questionButton" ).click(function() {
 
@@ -27,40 +25,5 @@ $(document).ready(function(){
 
 	});
 
-
-
-
 $("#answer").hide();
 });//closing of jquery
-
-
-/*//jquery wrap
-$(document).ready(function() {
-
-//define all the parts
-var magic8Ball = {};
-var question = function() { prompt("What yes / no question do you want to ask the Magic 8 Ball?");
-};
-//make sure no answer is showing when page loads
-$("#answer").hide();
-
-//user clicks the ASK ME ANYTHING BUTTON
-$("#questionButton").click(question);
-
-$("#answer").hide();
-
-magic8Ball.listOfAnswers = ["Maybe", "That's Possible", "For Sure", "With Certainty", "That's a NO", "Ain't Happening", "Make it So", "Sorry, Ask Again"];
-
-//get the random number
-var random = Math.floor(Math.random()* magic8Ball.listOfAnswers.length);
-
-//use the random number to grab from the array
-var showAnswer = magic8Ball.listOfAnswers[random];
-
-//write the answer to the screen
-
-$("#answer").text(showAnswer);
-$("#answer").fadeIn(3000);
-
-
-});*/
